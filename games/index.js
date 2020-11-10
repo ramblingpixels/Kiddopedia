d5bda3a27f511349bb862924f66bff354dcf1a05
ramblingpixels <55074331+ramblingpixels@users.noreply.github.com>
2020-11-10 22:52:24 +0530

    Initial files uploaded

---
+++ games/index.js
@@ -0,0 +1,19 @@
+$(function ()){
+  "use strict";
+  var anim id;
+
+  // Saving the DOM objects to variables
+  var container   = $('#container'),
+   line           = $('#line'),
+   line_1         = $('#line-1'),
+   line_2         = $('#line-2'),
+   line_3         = $('#line-3'),
+   car            = $('#car'),
+   car_1          = $('#car-1'),
+   car_2          = $('#car-2'),
+   car_3          = $('#car-3'),
+   restart_div    = $('#restart-div'),
+   restart_btn    = $('#restart'),
+   score          = $('#score');
+
+var container_left = parseInt(container.css('left')),
